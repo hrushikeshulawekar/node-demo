dc51fb70b34740c4b8d153e125e8e3214dfb45d7
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-16 17:43:33 +0530

    Update demo description to version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This cicd pipeline app demo.
+          This cicd pipeline app demo - it is version 2.
         </p>
         <a
           className="App-link"
