7683ffbb6b563a6414a009c5eef62038aa6e8b3a
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-17 15:58:50 +0530

    again back to node port

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This cicd pipeline app demo - LoadBalancer check.
+          This cicd pipeline app demo - NodePort back.
         </p>
         <a
           className="App-link"
