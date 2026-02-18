6607868c7d08744492b5fb6edb730c93999d0630
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-18 16:24:57 +0530

    checked in another virtual machine

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          cicd pipeline - NodePort back - Thanks.
+          cicd pipeline - completely fine.
         </p>
         <a
           className="App-link"
