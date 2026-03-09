d33413ce52d71ff3c44bc352a20b614ec4666a0b
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-03-09 15:41:02 +0530

    just check working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          cicd pipeline - let's confirm it.
+          cicd pipeline - is it working now?
         </p>
         <a
           className="App-link"
