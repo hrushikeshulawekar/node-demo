a06b20352011d215f9a5e0d8e26ed46efdde135a
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-06-08 17:29:18 +0530

    rollback test completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test roll back.
+          Test roll back-completed.
         </p>
         <a
           className="App-link"
