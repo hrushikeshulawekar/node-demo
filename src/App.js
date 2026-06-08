f4a26282aa0c05554d63dc9715fc7b2335e146ce
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-06-08 16:18:41 +0530

    back to the normal

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  throw new Error("Rollback Test");
-
-  return <div>Hello</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          cicd pipeline --status -confirmed 07/06/2026
+        </p>
+        
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
