e5832329b4d0959aae4c04f2d3a51a3bdc78024d
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-06-08 13:33:08 +0530

    tried to fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // This will crash the app at runtime inside the container
-  throw new Error("🔴 Intentional crash to test rollback!");
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          cicd pipeline --ROLLBACK TEST 🔴
+          cicd pipeline --status -confirmed 07/06/2026
         </p>
+        
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
