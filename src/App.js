cbc733a70f9d3115bf54bc08a64966e9e8fd8517
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-06-08 16:08:09 +0530

    rollback test

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          call it a day, confirmed on 27-02-2026.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  throw new Error("Rollback Test");
+
+  return <div>Hello</div>;
 }
 
 export default App;
